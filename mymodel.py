66ac87424ecbcd5b4ff22730d9bf5a2257e1f5a2
Jordan Zhang <taoran.z96@gmail.com>
2021-03-12 00:13:28 -0800

    1

--- mymodel.py
+++ mymodel.py
@@ -2,15 +2,16 @@ import os
 from pathlib import Path
 import pandas as pd
 import numpy as np
-from statsmodels.iolib.smpickle import load_pickle
+import pickle
 
 def SalePrediction(form_dict):
 
     repo_path = Path(os.getcwd())
-    lm = load_pickle(repo_path / "sale_predictor.pickle")
+    lm = pickle.load(repo_path / "model.p")
+    enc= pickle.load(repo_path / "encoder.p")
     try:
         df = pd.DataFrame(form_dict, index=[0])
-        pred_sale = lm.predict(df)['predicted_Gross'][0]
+        pred_sale = lm.predict(enc.transform(df))[0]
 
 
         return "The show is expected to generate revenue of ${1}!".format(pred_sale)
